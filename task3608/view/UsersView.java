082b95b4ab6f2388211be1c028bc93cf8e1a31b1
self-harm <69676561+self-harm@users.noreply.github.com>
2020-11-22 22:54:17 +0300

    Add files via upload

---
+++ task3608/view/UsersView.java
@@ -0,0 +1,39 @@
+package com.javarush.task.task36.task3608.view;
+
+import com.javarush.task.task36.task3608.bean.User;
+import com.javarush.task.task36.task3608.controller.Controller;
+import com.javarush.task.task36.task3608.model.ModelData;
+
+public class UsersView implements View{
+    private Controller controller;
+
+    @Override
+    public void refresh(ModelData modelData) {
+            System.out.println("All " + (modelData.isDisplayDeletedUserList()==true ? "deleted " : "") + "users:");
+            for (User each : modelData.getUsers()) {
+                System.out.println("\t" + each);
+                }
+            System.out.println("===================================================");
+    }
+
+    @Override
+    public void setController(Controller controller) { //setter
+        this.controller = controller;
+    }
+
+    public void fireEventShowAllUsers() {
+        controller.onShowAllUsers();
+    }
+
+    public void fireEventShowDeletedUsers() {
+        controller.onShowAllDeletedUsers();
+    }
+
+    public void fireEventOpenUserEditForm(long id) {
+    controller.onOpenUserEditForm(id);
+    }
+
+
+
+
+}
