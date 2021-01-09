3328a6c2746300bb7b16aae979a0ffa74218829b
self-harm <lightgraywrite@gmail.com>
2021-01-10 01:17:08 +0300

    Create Employee.java

---
+++ mvc_practice/mvc_project/src/main/java/io/harmed/spring/mvc/Employee.java
@@ -0,0 +1,53 @@
+package io.harmed.spring.mvc;
+
+public class Employee {
+    private String name;
+    private String surname;
+    private int salary;
+    private String department;
+
+    public Employee() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", salary=" + salary +
+                ", department='" + department + '\'' +
+                '}';
+    }
+}
